6563850b10c57adaa659d9e675daa39a62caff7b
Aadithya A R <aadithyaar22@gmail.com>
2026-04-26 22:03:06 +0530

    fix: remove optimizeCss to fix critters build error

--- next.config.ts
+++ next.config.ts
@@ -7,9 +7,6 @@ const nextConfig: NextConfig = {
       { protocol: 'https', hostname: 'github-readme-stats.vercel.app' },
     ],
   },
-  experimental: {
-    optimizeCss: true,
-  },
 }
 
 export default nextConfig
